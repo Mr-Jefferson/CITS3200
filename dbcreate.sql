12bba777239e6b489a60a2514b8b8f59cfd9e2c9
Luke Davis <21128099@student.uwa.edu.au>
2014-08-19 16:35:34 +0800

    few fixes

--- dbcreate.sql
+++ dbcreate.sql
@@ -7,7 +7,7 @@ USE seminarmarks;
 -- Table Students
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Students (
-  idStudent INT NOT NULL,
+  idStudent INT NOT NULL auto_increment,
   StudentFirstName VARCHAR(45) NOT NULL,
   StudentLastName VARCHAR(45) NOT NULL,
   StudentNumber VARCHAR(45) NULL,
@@ -18,39 +18,41 @@ CREATE TABLE IF NOT EXISTS Students (
 -- Table Markers
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Markers (
-  idMarker INT NOT NULL,
+  idMarker INT NOT NULL auto_increment,
   MarkerFirstName VARCHAR(45) NOT NULL,
   MarkerLastName VARCHAR(45) NOT NULL,
   PRIMARY KEY (idMarker));
   
-  
-  -- -----------------------------------------------------
--- Table Marks
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Marks (
-  idMark INT NOT NULL,
-  Mark1 VARCHAR(45) NOT NULL,
-  Mark2 VARCHAR(45) NOT NULL,
-  Mark3 VARCHAR(45) NOT NULL,
-  Cohor VARCHAR(45) NOT NULL,
-  Seminar VARCHAR(45) NOT NULL,
-  
-  
-  idStudent VARCHAR(45) NOT NULL,
-  idMarker VARCHAR(45) NOT NULL,
-  idUser VARCHAR(45) NOT NULL,
-  
-  
-  PRIMARY KEY (idMark));
- 
-  
+
 
 -- -----------------------------------------------------
 -- Table Users
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Users (
-  idusers INT NOT NULL,
+  idUser INT NOT NULL auto_increment,
   username VARCHAR(45) NOT NULL,
   password VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idusers),
-  UNIQUE INDEX username_UNIQUE (username ASC));
+  UNIQUE (username),
+  PRIMARY KEY (idUser));
+  
+  -- -----------------------------------------------------
+-- Table Marks
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS Marks (
+  idMark INT NOT NULL auto_increment,
+  Mark1 INT NOT NULL,
+  Mark2 INT NOT NULL,
+  Mark3 INT NOT NULL,
+  Cohort VARCHAR(45) NOT NULL,
+  Seminar VARCHAR(45) NOT NULL,
+  PRIMARY KEY (idMark),
+  
+  idStudent INT NOT NULL,
+  idMarker INT NOT NULL,
+  idUser INT NOT NULL,
+  
+  FOREIGN KEY (idStudent) REFERENCES Students(idStudent),
+  FOREIGN KEY (idMarker) REFERENCES Markers(idMarker),
+  FOREIGN KEY (idUser) REFERENCES Users(idUser));
+ 
+  
